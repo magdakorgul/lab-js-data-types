9c3ad4b59ab61cbad1bd80fae4b59d86fd049e36
magdakorgul <magdalenakorgul1@gmail.com>
2024-04-10 20:50:49 +0200

    Individual labs ready

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const names = s1+ ' ' + s2 + ' ' + s3 + ' ' + s4;
+console.log(names);
+const tongueTwister = names + ' ' + s5 + ' ' + s3 + ' ' + s2 + ' ' + s1 + ' ' + s4 + ".";
 
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,11 +22,19 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let cameLtaiL = part1 + part2;
+console.log(cameLtaiL);
 
+cameLtaiL = cameLtaiL.split('');
+cameLtaiL.splice(3, 1, "A");
+cameLtaiL = cameLtaiL.join('');
+console.log(cameLtaiL);
 
+cameLtaiL = cameLtaiL.split('');
+cameLtaiL.splice(9, 1, 'T');
+cameLtaiL = cameLtaiL.join('');
 // Print the cameLtaiL-formatted string
-
-
+console.log(cameLtaiL);
 
 
 /*******************************************
@@ -35,21 +43,21 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+tipAmount = billTotal*0.15;
 
 // Print out the tipAmount
-
-
-
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+for (i=1; i<=10; i++);
+let randomNumber = Math.random()*10;
 
 // Print the generated random number
+console.log(Math.round(randomNumber));
 
 
 
@@ -62,15 +70,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// expression1 =false
+console.log(expression1);
 
 const expression2 = a || b;
+// expression2 = true;
+console.log(expression2);
 
 const expression3 = !a && b;
+// expression3 = false
+console.log(expression3);
 
 const expression4 = !(a && b);
+// expression4 = true;
+console.log(expression4);
 
 const expression5 = !a || !b;
+// expression5 = true
+console.log(expression5);
 
 const expression6 = !(a || b);
+// expression6 = false
+console.log(expression6);
 
 const expression7 = a && a;
+// expression7 = true
+console.log(expression7);
